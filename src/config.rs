25736521b31ba3d2aa29ac050b66bdaa23e47180
lispcat <187922791+lispcat@users.noreply.github.com>
2025-04-07 18:49:04 -0400

    custom deserializer for Components: Vec<Box<dyn Component>>

--- src/config.rs
+++ src/config.rs
@@ -1,26 +1,25 @@
 use anyhow::Result;
-use serde::Deserialize;
+use serde::{Deserialize, Deserializer};
 use serde_json::Value;
 use smart_default::SmartDefault;
 use std::{collections::HashMap, error::Error, fs, path::PathBuf};
 
 use crate::{
-    Args,
     components::{
-        Component,
         alsa::{Alsa, AlsaSettings},
         backlight::{Backlight, BacklightSettings},
         battery::{Battery, BatterySettings},
         time::{Time, TimeSettings},
+        Component,
     },
+    Args,
 };
 
-#[derive(SmartDefault, Debug)]
+#[derive(SmartDefault, Debug, Deserialize)]
 pub struct Config {
     pub settings: Settings,
 
-    #[default(Vec::new())]
-    pub components: Vec<Box<dyn Component>>,
+    pub components: Components,
 }
 
 #[derive(SmartDefault, Debug, Deserialize)]
@@ -33,20 +32,22 @@ pub struct Settings {
     pub default_separator: String,
 }
 
-// a method for Config that parses a component and returns a new instance of it
-macro_rules! parse_components {
+#[derive(SmartDefault, Debug)]
+#[default(Vec::new())]
+pub struct Components(pub Vec<Box<dyn Component>>);
+
+macro_rules! component_parser {
     ($(($name:expr, $settings_type:ident, $component_type:ident)),*) => {
-        fn parse_component(key: &String, value: &Value) -> Result<Box<dyn Component>, Box<dyn Error>> {
+        fn component_parser(key: &String, value: &Value) -> Result<Box<dyn Component>, Box<dyn Error>> {
             match key.to_lowercase().as_str() {
                 $(
                     $name => {
                         let settings: $settings_type = serde_json::from_value(value.clone())
-                            .unwrap_or_else(|_| panic!("failed to parse {} config", key));
-                        let component = $component_type {
+                            .map_err(|_| format!("failed to parse {} config", key))?;
+                        Ok(Box::new($component_type {
                             settings,
                             ..Default::default()
-                        };
-                        Ok(Box::new(component))
+                        }))
                     }
                 )*,
                 _ => Err(format!("can't parse unknown component {}", key).into()),
@@ -55,14 +56,46 @@ macro_rules! parse_components {
     };
 }
 
-// TODO: use RON instead of json
+impl Components {
+    component_parser!(
+        ("alsa", AlsaSettings, Alsa),
+        ("backlight", BacklightSettings, Backlight),
+        ("battery", BatterySettings, Battery),
+        ("time", TimeSettings, Time)
+    );
+}
+
+/// make Components deserializable
+impl<'de> Deserialize<'de> for Components {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        // Deserialize into a HashMap first
+        let components_map: HashMap<String, Value> = HashMap::deserialize(deserializer)?;
+
+        // Parse each component
+        let component_vec = components_map
+            .iter()
+            .map(|(component_name, settings)| {
+                Components::component_parser(component_name, settings).map_err(|e| {
+                    serde::de::Error::custom(format!(
+                        "could not parse component {}: {}",
+                        component_name, e
+                    ))
+                })
+            })
+            .collect::<Result<Vec<Box<dyn Component>>, D::Error>>()?;
+
+        Ok(Components(component_vec))
+    }
+}
 
 impl Config {
     pub fn new(args: &Args) -> Result<Self, Box<dyn Error>> {
         let path: PathBuf = args.config_path.clone();
         let contents: String = Self::read_json(&path)?;
-        let deserialized: HashMap<String, Value> = Self::deserialize_json(&contents)?;
-        let config: Config = Self::parse_config(deserialized)?;
+        let config: Config = serde_json::from_str(&contents)?;
         Ok(config)
     }
 
@@ -70,58 +103,6 @@ impl Config {
     fn read_json(contents: &PathBuf) -> Result<String, Box<dyn Error>> {
         Ok(fs::read_to_string(contents).map_err(|_| "failed to read config file")?)
     }
-
-    // deserialize String json into HashMap
-    fn deserialize_json(contents: &str) -> Result<HashMap<String, Value>, Box<dyn Error>> {
-        Ok(serde_json::from_str(contents)?)
-    }
-
-    // parse HashMap and return generated Config struct
-    fn parse_config(hashmap: HashMap<String, Value>) -> Result<Config, Box<dyn Error>> {
-        hashmap.into_iter().try_fold(
-            Self::default(),
-            |acc_config, (category, body)| match category.to_lowercase().as_str() {
-                "components" => {
-                    let components = body
-                        .as_object()
-                        .ok_or_else(|| format!("could not parse category: {}", category))?
-                        .iter()
-                        .map(|(component_name, settings)| {
-                            Config::parse_component(component_name, settings).map_err(|_| {
-                                format!(
-                                    "could not parse component {}: {:?}",
-                                    component_name, settings
-                                )
-                            })
-                        })
-                        .collect::<Result<Vec<Box<dyn Component>>, _>>()?;
-
-                    Ok(Self {
-                        components,
-                        ..acc_config
-                    })
-                }
-                "settings" => {
-                    let settings = serde_json::from_value(body)
-                        .map_err(|_| format!("could not parse category {}", category))?;
-
-                    Ok(Self {
-                        settings,
-                        ..acc_config
-                    })
-                }
-                x => Err(format!("unknown setting category: {}", x).into()),
-            },
-        )
-    }
-
-    // parse a component and return a new instance of it
-    parse_components!(
-        ("alsa", AlsaSettings, Alsa),
-        ("backlight", BacklightSettings, Backlight),
-        ("battery", BatterySettings, Battery),
-        ("time", TimeSettings, Time)
-    );
 }
 
 // #[cfg(test)]
--- src/main.rs
+++ src/main.rs
@@ -58,9 +58,11 @@ mod tests {
             ..Args::default()
         })
         .expect("failed to get config");
-        for c in config.components.iter_mut() {
+        for c in config.components.0.iter_mut() {
             c.update().unwrap();
         }
-        println!("Config: {:#?}", config);
+        // TODO: implement an update method for each
+        // println!("TESTING: {:#?}", config);
+        println!("Config: {:#?}", config.components);
     }
 }
