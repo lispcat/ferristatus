e65c83a74ab231a49c432ae3dca4158ecad7e1d0
lispcat <187922791+lispcat@users.noreply.github.com>
2025-04-29 18:13:40 -0400

    main printer

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,39 @@
+use std::{thread, time::Duration};
+
+use anyhow::Context;
 use args::Args;
 use clap::Parser;
+use components::Component;
+use config::Config;
 
 mod args;
 mod components;
 mod config;
 
-fn main() {
+fn main() -> anyhow::Result<()> {
     // parse args
     let args = Args::parse();
 
     // load config
+    let mut config = Config::new(&args)
+        .map_err(|e| anyhow::anyhow!("crocdile crocodale -- {e}"))?;
 
     // run
-    for _ in 0..10 {
-        println!("wah");
+    loop {
+        // update components
+        let val = config
+            .components
+            .vec
+            .iter_mut()
+            .map(|c| -> anyhow::Result<String> {
+                c.update().context("component update failed")?;
+                c.format().context("failed to format")
+            })
+            .collect::<Result<Vec<String>, _>>()?
+            .join(&config.settings.default_separator);
+        thread::sleep(Duration::from_millis(config.settings.check_interval));
+
+        println!("{}", val);
     }
 }
 
@@ -29,10 +49,8 @@ mod tests {
             config_path: "examples/config.yml".into(),
         };
 
-        let mut config = match Config::new(&args) {
-            Ok(c) => c,
-            Err(e) => anyhow::bail!("crocdile crocodale -- {e}"),
-        };
+        let mut config = Config::new(&args)
+            .map_err(|e| anyhow::anyhow!("crocdile crocodale -- {e}"))?;
 
         for _ in 0..10 {
             println!(
@@ -48,7 +66,8 @@ mod tests {
                     .collect::<Vec<String>>()
                     .join(&config.settings.default_separator)
             );
-            thread::sleep(Duration::from_secs(1));
+            // TODO: make it only print when at least one component is actually updated
+            thread::sleep(Duration::from_millis(config.settings.check_interval));
         }
         Ok(())
     }
